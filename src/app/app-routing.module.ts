047ded49b426116db2e866eb1d41d00e0437d3e0
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-06-22 02:47:22 +1000

    Cleaned up minor things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { HomeComponent } from './home/home.component';
-import { IronThroneComponent } from './iron-throne/iron-throne.component';
-import { IronThroneCandidatesComponent } from './iron-throne-candidates/iron-throne-candidates.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  // { path: 'iron-throne-candidates', component: IronThroneCandidatesComponent },
-  // { path: 'iron-throne/:id', component: IronThroneComponent }
 ];
 
 @NgModule({
